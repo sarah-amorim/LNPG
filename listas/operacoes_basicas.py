c710ddbf85ac8ab3d7e8394ee133c70220bacf28
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-08-14 15:29:12 -0300

    Update operacoes_basicas.py

--- listas/operacoes_basicas.py
+++ listas/operacoes_basicas.py
@@ -1,1 +1,11 @@
-# hello world
+# Crie uma lista vazia chamada frutas e adicione nela, nesta ordem, as seguintes frutas: "maçã", "banana" e "laranja"
+
+# Na lista numeros = [1, 2, 4, 5], insira o número 3 na posição correta para manter a sequência numérica.
+
+# Dada a lista itens = ["teclado", "mouse", "monitor", "mouse"], remova apenas o primeiro item "mouse" da lista.
+
+# Verifique se a string "carro" está presente na lista veiculos = ["moto", "bicicleta", "carro", "ônibus"]. Imprima uma mensagem apropriada.
+
+# Dada a lista nomes = ["Ana", "Beatriz", "Carlos"], remova o último elemento da lista usando um método apropriado e armazene o valor removido em uma variável chamada removido.
+
+# Crie uma lista com 50 elementos randômicos, ordene a lista in-place e imprima a nova lista:

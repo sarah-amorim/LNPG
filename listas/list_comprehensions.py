939a7eedaf0817bbc1891bd26ffddf13dd8db36f
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-08-14 15:31:56 -0300

    Update list_comprehensions.py

--- listas/list_comprehensions.py
+++ listas/list_comprehensions.py
@@ -1,1 +1,9 @@
-# hello world
+# Usando list comprehension, crie uma lista com as letras da palavra "informática" exceto as vogais.
+
+# Dada a matriz matriz = [[1, 2], [3, 4], [5, 6]], crie uma lista achatada com todos os elementos da matriz.
+
+# Dada a lista valores = [3, -2, 7, -1, 0], crie uma nova lista onde números negativos são substituídos por 0 e os demais permanecem inalterados.
+
+# Dada a string "a1b2c3d4", use list comprehension para criar uma lista com os números inteiros extraídos da string.
+
+# Dada a lista numeros = list(range(20)), crie uma nova lista contendo o dobro de todos os números ímpares maiores que 5 e menores que 20.

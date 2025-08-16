5662a7154c08784c54451e36698a89fe0dea2311
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-08-16 17:21:48 -0300

    Update slices.py

--- listas/slices.py
+++ listas/slices.py
@@ -1,9 +1,15 @@
 # Dada a lista letras = ["a", "b", "c", "d", "e", "f"], obtenha uma nova lista com os elementos do índice 1 até o 3 (inclusive o 1, exclusivo o 4).
-
+letras = ["a", "b", "c", "d", "e", "f"]
+letras[1:4]
 # Com a lista numeros = [10, 20, 30, 40, 50], use slicing para obter os três primeiros elementos.
-
+numeros = [10, 20, 30, 40, 50]
+numeros[:3]
 # A partir da lista valores = [1, 2, 3, 4, 5, 6], crie uma nova lista contendo os elementos a partir do índice 3.
-
+valores = [1, 2, 3, 4, 5, 6]
+novos_valores = valores[3:]
 # Dada a lista pares = [0, 2, 4, 6, 8, 10, 12], crie uma nova lista pegando um elemento a cada dois.
-
+pares = [0, 2, 4, 6, 8, 10, 12]
+novos_pares = pares[::2]
 # Dada a lista nomes = ["Ana", "Bea", "Caio", "Duda"], use slicing para criar uma nova lista invertida.
+nomes = ["Ana", "Bea", "Caio", "Duda"]
+nomes[::-1]

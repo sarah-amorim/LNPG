6dbac276f1ae345daf8f2f15e6e66dd367e963f2
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-10-02 14:28:57 -0300

    "update"

---
+++ arquivos/lojas.py
@@ -0,0 +1,20 @@
+'''
+1. Listar os nomes de todas as lojas.
+(Percorra dados["lojas"] e imprima ["nome"].)
+
+2. Mostrar todos os produtos da loja "Loja Centro".
+(Filtre pelo nome da loja, depois percorra "produtos".)
+
+3. Gerar um CSV com as colunas:
+nome_loja, nome_cliente, id_pedido, data_pedido.
+
+4. Gerar outro CSV com as colunas:
+nome_loja, nome_produto, categoria, preco.
+(Ou seja, um catálogo de produtos de todas as lojas.)
+
+5. Mostrar os nomes dos clientes com idade acima de 25 anos em qualquer loja.
+
+6. Listar todos os pedidos, mostrando o nome do cliente e os nomes dos produtos comprados.
+
+7. Descobrir qual é o produto mais caro vendido em todas as lojas.
+'''
